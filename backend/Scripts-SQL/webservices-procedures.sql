1f494f27e0e1728eb6807c8b88e3739ed2b42e4b
sebastienvolont <64020075+sebastienvolont@users.noreply.github.com>
2020-04-27 15:26:29 +0200

    Update webservices-procedures.sql

--- backend/Scripts-SQL/webservices-procedures.sql
+++ backend/Scripts-SQL/webservices-procedures.sql
@@ -1,8 +1,38 @@
-CREATE PROCEDURE "DBA"."http_getCSS"(in url char(255))
+/*----------------------------------------------------------------PROCEDURES----------------------------------------------------------------*/
+CREATE PROCEDURE "http_getPage"in url char(255))
 BEGIN
+    call sa_set_http_header('Content-Type', 'text/html; charset=utf-8');
+    call sa_set_http_header('Access-Control-Allow-Origin', '*');
+    select xp_read_file(dba.getPath() || url || '.html');
+END;
 
+CREATE PROCEDURE "http_getCSS"(in url char(255))
+BEGIN
   call sa_set_http_header('Content-Type', 'text/css');
   call sa_set_http_header('Access-Control-Allow-Origin', '*');
-	select xp_read_file(dba.getPath() || 'CSS\' || url);
-                      
+  select xp_read_file(dba.getPath() || 'css\' || url);                      
 END;
+
+CREATE PROCEDURE "http_getJS"(in url char(255))
+BEGIN
+  call sa_set_http_header('Content-Type', 'application/javascript');
+  call sa_set_http_header('Access-Control-Allow-Origin', '*'); 
+  select xp_read_file(dba.getPath() || 'js\' || url);
+END;
+
+CREATE PROCEDURE "http_getIMG"(in url char(255))
+BEGIN
+  call sa_set_http_header('Content-Type', 'image/png');
+  call sa_set_http_header('Access-Control-Allow-Origin', '*');
+  select xp_read_file(dba.getPath() || 'img\' || url);
+END;
+
+/*----------------------------------------------------------------Webservices--------------------------------------------------------------*/
+
+CREATE SERVICE "page" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call dba.http_getPage(:url);
+
+CREATE SERVICE "js" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call dba.http_getJS(:url);
+
+CREATE SERVICE "css" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call dba.http_getCSS(:url);
+
+CREATE SERVICE "img" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call dba.http_getIMG(:url);
