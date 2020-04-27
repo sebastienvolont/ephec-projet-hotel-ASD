d04e085ed110041e1df5733c5c8cde3db2bafaf2
sebastienvolont <64020075+sebastienvolont@users.noreply.github.com>
2020-04-27 15:12:28 +0200

    Update webservices-procedures.sql

--- backend/Scripts-SQL/webservices-procedures.sql
+++ backend/Scripts-SQL/webservices-procedures.sql
@@ -1,1 +1,8 @@
+CREATE PROCEDURE "DBA"."http_getCSS"(in url char(255))
+BEGIN
 
+  call sa_set_http_header('Content-Type', 'text/css');
+  call sa_set_http_header('Access-Control-Allow-Origin', '*');
+	select xp_read_file(dba.getPath() || 'CSS\' || url);
+                      
+END;
