36bc1593309a61dfcf4417b9b25e51a500393e04
sebastienvolont <64020075+sebastienvolont@users.noreply.github.com>
2020-05-10 21:25:18 +0200

    Update creation-tables.sql

--- backend/scripts-base-de-donnees/creation-tables.sql
+++ backend/scripts-base-de-donnees/creation-tables.sql
@@ -1,1 +1,14 @@
 
+CREATE TABLE "ACTIVITEES" (
+    "idActivitee" char(10) NOT NULL PRIMARY KEY,
+    "nomActivitee" CHAR(50)NOT NULL,
+    "nbrPersonne" INTEGER NOT NULL,
+    "prix" INTEGER NOT NULL,
+    "typeActivitee" CHAR(20)NOT NULL,
+    "jourSemaine" CHAR(20)NOT NULL,
+    "horaires" char(30)  NOT NULL,
+    "idAgent" char(10) NOT NULL,
+    FOREIGN KEY (IdAgent) REFERENCES AGENTS(IdAgent)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
