00e79ff616b15e1bef99496097713d98e3ccfae2
sebastienvolont <64020075+sebastienvolont@users.noreply.github.com>
2020-05-10 23:55:12 +0200

    Update code_reservationHotel_descriptCH.js

--- backend/scripts-api-rest/code_reservationHotel_descriptCH.js
+++ backend/scripts-api-rest/code_reservationHotel_descriptCH.js
@@ -1,1 +1,14 @@
 
+/*Procedure descriptionChambres pour récupérer les données dans le table chambre*/
+
+ALTER PROCEDURE "DBA"."descriptionChambres"()
+ RESULT( detailChambre CHAR(500),nombreLit INTEGER, prixChambre INTEGER)
+BEGIN
+    CALL sa_set_http_header('Access-Control-Allow-Origin','*');
+    CALL sa_set_http_header('Content-Type', 'application:json; charset-utf-8');
+    SELECT c.Description, c.nbreLit, c.prixChambreParNuit  
+    FROM  CHAMBRES c
+END
+
+/*Web service descriptionChambre*/
+call descriptionChambres()
