75cfe80878ee39bb57093fbe7fadbf447c40e779
sebastienvolont <64020075+sebastienvolont@users.noreply.github.com>
2020-05-10 21:25:37 +0200

    Update insertion-donnees.sql

--- backend/scripts-base-de-donnees/insertion-donnees.sql
+++ backend/scripts-base-de-donnees/insertion-donnees.sql
@@ -1,1 +1,22 @@
-
+INSERT INTO ACTIVITEES (idActivitee,nomActivitee, nbrPersonne, prix, typeActivitee, jourSemaine, horaires,idAgent)
+VALUES
+    ('Acti01','Escalade', 4 , 5, 'Sport', 'Lundi', '08h00 à 9h40','A3'),
+    ('Acti02','Visite de la ville', 8 , 20, 'Divertissement', 'Lundi', '10h00 à 14h00','A4'),
+    ('Acti03','Spectacle', 30, 6, 'Divertissement', 'Lundi', '19h45 à 22h00','A4'),
+    ('Acti04','Kayak', 10, 12, 'Sport', 'Mardi', '09h15 à 12h45','A4'),
+    ('Acti05','Cours de yoga', 6 , 3, 'Divertissement', 'Mardi', '14h00 à 15h00','A3'),
+    ('Acti06','Volley Ball', 10, 4, 'Sport', 'Mardi', '15h30 à 17h30','A3'),
+    ('Acti07','Randonnée', 10, 16, 'Divertissement', 'Mercredi', '09h00 à 13h00','A4'),
+    ('Acti08','Tournoi de Ping-pong', 12 , 5, 'Sport', 'Mercredi', '16h00 à 17h00','A3'),
+    ('Acti09','Soirée karaoké', 30, 4, 'Divertissement', 'Mercredi', '20h00 à 22h30','A3'),
+    ('Acti10','Initiation au surf', 6 , 9, 'Sport', 'Jeudi', '10h00 à 12h00','A4'),
+    ('Acti11','Tennis', 4 , 5, 'Sport', 'Jeudi', '14h00 à 16h00','A3'),
+    ('Acti12','Parties de poker', 4, 7, 'Divertissement', 'Jeudi', '20h00 à 22h00','A3'),
+    ('Acti13','Matinée à la mer', 8, 10, 'Divertissement', 'Vendredi', '07h00 à 11h30','A4'),
+    ('Acti14','Promenade en vélo', 15, 12, 'Sport', 'Vendredi', '14h00 à 18h00','A4'),
+    ('Acti15','Soirée cinéma', 25 , 8, 'Divertissement', 'Vendredi', '20h00 à 22h00','A3'),
+    ('Acti16','Pétanque', 6, 3, 'Sport', 'Samedi', '10h00 à 11h30','A4'),
+    ('Acti17','Mini-golf', 4, 5, 'Divertissement', 'Samedi', '14h00 à 17h00','A3'),
+    ('Acti18','Soirée dansante', 30 , 6, 'Divertissement', 'Samedi', '20h00 à 00h00','A4'),
+    ('Acti19','Match de Waterpolo', 10 , 5, 'Sport', 'Dimanche', '10h00 à 12h00','A3'),
+    ('Acti20','Atelier peinture', 5, 6, 'Divertissement', 'Dimanche', '15h00 à 17h00','A4');
