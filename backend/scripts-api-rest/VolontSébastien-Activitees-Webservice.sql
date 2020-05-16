126ff50d28d13c9516f4a2882efddb7ab5f135dd
sebastienvolont <64020075+sebastienvolont@users.noreply.github.com>
2020-05-16 20:53:17 +0200

    Update VolontSébastien-Activitees-Webservice.sql

--- backend/scripts-api-rest/VolontSébastien-Activitees-Webservice.sql
+++ backend/scripts-api-rest/VolontSébastien-Activitees-Webservice.sql
@@ -1,1 +1,10 @@
 
+CREATE PROCEDURE "proc_getActivitee" (in jour char(30), in typeActi char(20))
+RESULT (nomActivitee char(50), nbrPersonne integer, prix integer, jourSemaine char(20), horaires char(50),nomAgent char(60), prenomAgent char(60))
+BEGIN 
+    call sa_set_http_header('Content-Type', 'application/json');
+    call sa_set_http_header('Access-Control-Allow-Origin', '*');
+    SELECT nomActivitee, nbrPersonne, prix, jourSemaine, horaires, t2.prenomAgent
+    from ACTIVITEES AS t1 join AGENTS as t2 on t1.IdAgent = t2.IdAgent 
+    where jour = jourSemaine and typeActi = typeActivitee
+END
