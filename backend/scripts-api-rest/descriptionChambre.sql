0c347b2e7b4b4ec0596457dad7d6b260cf470834
SergeKouvahe <60178180+sergino1981@users.noreply.github.com>
2020-05-16 19:54:05 +0200

    Add files via upload

---
+++ backend/scripts-api-rest/descriptionChambre.sql
@@ -0,0 +1,20 @@
+-- -KOUVAHE Folly Serge  HE201817--
+
+-- Procedure qui charge le contenu de la  page web  chambre depuis  de la base de données --
+ALTER PROCEDURE "DBA"."descriptionChambres"()
+ RESULT( detailChambre CHAR(500),nombreLit INTEGER, prixChambre INTEGER)
+BEGIN
+    CALL sa_set_http_header('Access-Control-Allow-Origin','*');
+    CALL sa_set_http_header('Content-Type', 'application:json; charset-utf-8');
+    SELECT c.Description, c.nbreLit, c.prixChambreParNuit  
+    FROM  CHAMBRES c
+END
+			
+
+-------------------------------------------------------------------- Création des webservices------------------------------------------------------------------------------------
+
+
+CREATE SERVICE "http_searchClient" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call searchClient(:email);
+
+
+
