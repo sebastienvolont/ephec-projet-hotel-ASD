c271c1c3fc5670385b08cbbd3ad4213177ee3224
SergeKouvahe <60178180+sergino1981@users.noreply.github.com>
2020-05-16 19:57:26 +0200

    Add files via upload

---
+++ backend/scripts-api-rest/KOUVAHE Serge updateClient.sql
@@ -0,0 +1,16 @@
+-- -KOUVAHE Folly Serge  HE201817--
+
+-- Procedure de modification des données du client--
+
+ALTER PROCEDURE "DBA"."updateClient"( IN idclient INTEGER , nom CHAR (50), prenom CHAR (50), dateNaiss DATE )
+    RESULT( idclient INTEGER , nom CHAR(50), pren CHAR (50), dateNaiss DATE ) 
+BEGIN
+    Call sa_set_http_header('Access-Control-Allow-Origin', '*');
+    UPDATE client c
+    SET nomClient = nom, prenomClient = prenom, clientDateNaissance = dateNaiss
+    WHERE c.id = idclient
+ END
+ 
+ -------------------------------------------------------------------- Création des webservices------------------------------------------------------------------------------------
+ 
+ CREATE SERVICE "updateClient" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call updateClient(:idclient,:nom,:prenom,:dateNaiss);
---
+++ backend/scripts-api-rest/KOUVAHE SergerechercheClient.sql
@@ -0,0 +1,16 @@
+-- -KOUVAHE Folly Serge  HE201817--
+
+ --Procédure qui fait la recherche du client à base de son mail  --
+
+ALTER PROCEDURE "DBA"."searchClient"( IN email CHAR (30) )
+    RESULT(idclt INTEGER , nomClient CHAR (50), prenomClient CHAR (50), clientDateNaissance  DATE, mail CHAR (30))
+BEGIN
+	Call sa_set_http_header('Access-Control-Allow-Origin', '*');
+    call sa_set_http_header('Content-Type', 'application:json; charset-utf-8');
+    SELECT c.id, c.nomClient, c.prenomClient,c.clientDateNaissance, c.mailClient  FROM client c
+    WHERE c.mailClient = email
+END
+
+-------------------------------------------------------------------- Création des webservices------------------------------------------------------------------------------------
+
+CREATE SERVICE "descriptionChambre" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call descriptionChambres();
