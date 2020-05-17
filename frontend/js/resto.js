1dfb1a653a3e8e8091784c3821bc76308c78c4b0
HE201787 <65119804+HE201787@users.noreply.github.com>
2020-05-17 17:32:25 +0200

    Update resto.js

--- frontend/js/resto.js
+++ frontend/js/resto.js
@@ -1,16 +1,16 @@
-//PARMENTIER CHARLES HE201787
-// Appel la fonction Date minimum
+//PARMENTIER Charles HE201787
+
+// Appel de la fonction DateMin()
+// déclaration de la variable formResto qui fait référence au formulaire
 function initPage(){
     let formResto = document.getElementById("formRestaurant");
     dateMin();
     console.log("loaded");
-
-
 };
 
-// Fonction qui crée la date minimum
+// Fonction qui définit la date actuelle en date minimum dans le formulaire
 function dateMin(){
-   let currentDate = new Date();
+    let currentDate = new Date();
 
     let formattedDate = currentDate.getFullYear() + "-" + (currentDate.getMonth() + 1) + "-" + currentDate.getDate();
 
@@ -23,20 +23,21 @@ function dateMin(){
     }
 };
 
-// Fonction qui fait référence au formulaire
+// Fonction utile
 function getId(id){
     return document.getElementById(id);
 };
-// Fonction reçoit les valeurs des champs nom et prénom du formulaire
+
+// Fonction qui recupere les valeurs des champs nom et prénom du formulaire, fait une concaténation puis retourne le resultat
 function  nomResa(){
-    
     let nomResa= String( getId("name").value+" "+ getId("firstName").value);
     console.log(nomResa);
     return nomResa;
-    
 };
 
-// Soumission de la page reservation
+// Soumission du formulaire
+// appel au webservice
+// création d'un message de confimation dès que la requête est correctement exécutée
 function resaResto() {
     event.preventDefault();
     let xhr = new XMLHttpRequest();
@@ -45,7 +46,9 @@ function resaResto() {
         + '&nbPersonnes='
         + formResto.personne.value
         + '&dateCheck='
-        + formResto.dateResto.value,
+        + formResto.dateResto.value
+        + '&numeroChambre='
+        + formResto.idChambre.value,
         true);
 
     xhr.onload = function check() {
@@ -59,5 +62,6 @@ function resaResto() {
     xhr.send();
     console.log("fait aussi");
     let message = "<p>votre réservation a bien été prise en compte</p>";
+
 }
 
